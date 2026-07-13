82efd202d7d392be92e2dc5a6493e0a64cf0b73e
IslamCabarli <icabarli@std.beu.edu.az>
2026-07-13 18:07:20 +0400

    feat(models): define Question model relationships and attributes

--- apps/backend/app/Models/Question.php
+++ apps/backend/app/Models/Question.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Question extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'interview_id', 'skill_id', 'content', 'order'
+    ];
+
+    public function interview(): BelongsTo
+    {
+        return $this->belongsTo(Interview::class);
+    }
+
+    public function skill(): BelongsTo
+    {
+        return $this->belongsTo(Skill::class);
+    }
+
+    public function answer(): HasOne
+    {
+        return $this->hasOne(Answer::class);
+    }
 }
